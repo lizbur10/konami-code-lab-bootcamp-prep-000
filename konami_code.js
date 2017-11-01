78fc8b5355c3650b3c0f024b601353a75bc564be
Liz Burton <liz.burton147@gmail.com>
2017-11-01 19:18:20 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,7 +5,7 @@ function init() {
   let index=0;
 
   function onKeyDownHandler(e) {
-    const key = parseInt(e.detail);
+    const key = parseInt(e.detail || e.which);
 
     if (key === code[index]) {
       index++;
